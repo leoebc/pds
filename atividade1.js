c7e1f4d23fcb0c9797356bc14d73a8b3ba84d838
leoebc <73316973+leoebc@users.noreply.github.com>
2024-03-25 21:45:09 -0300

    Add files via upload

---
+++ atividade1.js
@@ -0,0 +1,6 @@
+let a = 15, b = 8, c = 11, d = 16
+console.log((a > 10 && a < 20) || b === 5)
+console.log(a > 10 || b === 5)
+console.log((a > 10 && a < 20) || (b === 5) || (c === 11))
+console.log((a > 10 && a < 20) || (b === 5) || (c !== 11))
+console.log((a > 10) || (b === 5) || (c !== 10) || (d !== 20))
